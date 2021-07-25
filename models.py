5af30741faf5aecc15a10fc10b8bc5b635bb09f6
silas <silaswulff@gmail.com>
2021-07-25 12:00:57 +0100

    Better layout, removed left column. Added dynamic data to timeline

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from simaritan import db
 
 
@@ -18,23 +20,36 @@ class Incident(db.Model):
     id = db.Column(db.Integer)
     incident_no = db.Column(db.String(32), primary_key=True, index=True, unique=True)
     description = db.Column(db.String(512))
+    # DB relationship with tasks and events, backreference to incident number
+    tasks = db.relationship('Task', backref='incidentno', lazy='dynamic')
+    events = db.relationship('Event', backref='incidentno', lazy='dynamic')
+    impactstatement = db.relationship('ImpactStatement', backref='incidentno', lazy='dynamic')
 
 
 class ImpactStatement(db.Model):
-    id = db.Column(db.Integer)
-    incident_no = db.Column(db.String(32), primary_key=True, index=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    incident_no = db.Column(db.String(32), db.ForeignKey('incident.incident_no'), index=True)
     # incident_no = db.Column(db.String(32), primary_key=True, ForeignKey='Incident.incident_no', index=True,
     # unique=True)
-    description = db.Column(db.String(512))
+    body = db.Column(db.String(512))
     submitter = db.Column(db.String(32), index=True)
     # submitter = db.Column(db.String(32), index=True, ForeignKey='User.username')
 
 
 class Task(db.Model):
-    id = db.Column(db.Integer)
-    incident_no = db.Column(db.String(32), primary_key=True, index=True, unique=True)
-    # incident_no = db.Column(db.String(32), primary_key=True, ForeignKey='Incident.incident_no', index=True,
-    # unique=True)
-    description = db.Column(db.String(512))
+    id = db.Column(db.Integer, primary_key=True)
+    incident_no = db.Column(db.String(32), db.ForeignKey('incident.incident_no'), index=True)
+    body = db.Column(db.String(512))
     assignee = db.Column(db.String(32), index=True)
     # assignee = db.Column(db.String(32), index=True, ForeignKey='User.username')
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(512))
+    assignee = db.Column(db.String(32), index=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    incident_no = db.Column(db.String(32), db.ForeignKey('incident.incident_no'), index=True)
+
+    def __repr__(self):
+        return 'Body {}'.format(self.body)
--- simaritan/__init__.py
+++ simaritan/__init__.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import MetaData
+
 from config import Config
+
+# Naming conventions are added to get around SQLite database limitation around altering tables.
+# They are passed into the DB initialization
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# Define the Database, adding Metadata to get around the SQLite limitation around altering databases
+db = SQLAlchemy(app=app,metadata=MetaData(naming_convention=naming_convention))
+#db = SQLAlchemy(app)
+migrate = Migrate(app, db, render_as_batch=True)
 
 from simaritan import routes
 import models
