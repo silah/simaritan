628859db657c9776a3d6cf5806780088260db5e0
silas <silaswulff@gmail.com>
2021-08-28 11:40:47 +0100

    Replaced Trello with Task export

--- simaritan/routes.py
+++ simaritan/routes.py
@@ -253,9 +253,9 @@ def submitimpact(incident):
                                impacts=impacts, inc=inc, impactf=form)
 
 
-@app.route('/report/<incident>')
+@app.route('/report/event/<incident>')
 @login_required
-def report(incident):
+def report_event(incident):
     # Get the timeline for the CSV file
     timeline = Event.query.filter_by(incident_no=incident).order_by(Event.timestamp.desc()).all()
     # Start with a blank string
@@ -264,7 +264,27 @@ def report(incident):
         csv += '\n{},{},{},{}'.format(t.timestamp, t.assignee, t.activity, t.body)
 
     response = make_response(csv)
-    cd = 'attachment; filename=report_{}'.format(incident)
+    cd = 'attachment; filename=event_report_{}'.format(incident)
+    response.headers['Content-Disposition'] = cd
+    response.mimetype = 'text/csv'
+
+    return response
+
+
+@app.route('/report/tasks/<incident>')
+@login_required
+def report_task(incident):
+    # Get the tasks for the CSV file
+    tasks = Task.query.filter_by(incident_no=incident).all()
+    # Start with a blank string
+    csv = 'ETA,Assignee,Description'
+    for t in tasks:
+        # If task is open, add to CSV
+        if t.status == "0":
+            csv += '\n{},{},{}'.format(t.eta, t.assignee, t.body)
+
+    response = make_response(csv)
+    cd = 'attachment; filename=open_tasks_{}'.format(incident)
     response.headers['Content-Disposition'] = cd
     response.mimetype = 'text/csv'
 
